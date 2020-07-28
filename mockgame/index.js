2639266ec5e5cb596996e323f68333e40cb22a73
Harry <krugerh27@gmail.com>
2020-07-28 13:02:17 +0200

    Adding testing

--- mockgame/index.js
+++ mockgame/index.js
@@ -2,12 +2,23 @@ const URL = 'http://localhost:3000/';
 
 const Axios = require('axios');
 const axios = Axios.create({
-	baseURL: URL
+	baseURL: URL,
 });
 
 async function test() {
-	const testRes = await axios.get('/');
+	console.time('request');
+	console.log('request sent');
+	let testRes;
+	try {
+		testRes = await axios.get('/poll')
+	}
+	catch(err) {
+		console.log('failed request');
+		return console.log(err);
+	}
+	console.timeEnd('request');
 	console.log(testRes.data);
+	console.log(testRes.headers);
 }
 
 test();
