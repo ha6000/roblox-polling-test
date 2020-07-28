ca27f300ad4f2cd0793d2ac2df031cbc336e18cd
Harry <krugerh27@gmail.com>
2020-07-28 15:35:42 +0200

    A little prototype I spun up

--- game/polling.lua
+++ game/polling.lua
@@ -1,10 +1,45 @@
 local HttpService = game:GetService("HttpService")
+local SERVERURL = "http://localhost:3000"
 
-local POLLURL = "http://localhost:3000/poll"
+local module = {
+	Message = BindableEvent()
+}
 
-local response = HttpService:RequestAsync({
-	Url = POLLURL + "?id=" + game.JobId,
-	Method = "GET"
-})
 
-print(response.Body)
+module.ResponseMessage = {
+	data = "",
+	id = ""
+}
+
+function module.ResponseMessage:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function module.ResponseMessage:reply(message)
+	return HttpService.RequestAsync({
+		Url = SERVERURL + "/respond?game=" + game.JobId,
+		Headers = {
+			["Response-Id"] = self.id
+		}
+	})
+end
+
+function module:sendPollRequest()
+	local success, response = pcall(HttpService:RequestAsync({
+		Url = SERVERURL + "/poll?game=" + game.JobId,
+		Method = "GET"
+	}))
+	if success then
+		local message = ResponseMessage:new({
+			data = response.Body,
+			id = response.Headers["Response-Id"]
+		})
+		self.Message.Fire(message)
+		return message
+	else return nil
+end
+
+return module
