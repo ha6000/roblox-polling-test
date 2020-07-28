a0d32c19d1b5bcc12ca8b4bd37907587d2ba2f05
Harry <krugerh27@gmail.com>
2020-07-28 13:02:26 +0200

    Addig poll timeout

--- server/index.js
+++ server/index.js
@@ -5,14 +5,17 @@ const app = express();
 
 const PACKAGE = path.join(__dirname, 'package.json');
 
+const POLLTIMEOUT = 12 * 60 * 1000;
+
 app.get('/', (req, res) => {
 	res.sendFile(PACKAGE);
 })
 
 app.get('/poll', (req, res) => {
-	res.send('test');
+	req.socket.setTimeout(POLLTIMEOUT);
+	setTimeout(() => {res.send('test')}, 5 * 60 * 1000);
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
 	console.log('Listening on port 3000');
 });
